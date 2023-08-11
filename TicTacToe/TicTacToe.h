9c0d0564cb8704d30421b669d248e43cc5192f7e
Peter Combee <petercombee@gmail.com>
2023-08-11 15:08:05 +0200

    Initial Commit

---
+++ TicTacToe/TicTacToe.h
@@ -0,0 +1,18 @@
+//
+//  TicTacToe.h
+//  TicTacToe
+//
+//  Created by Peter Combee on 11/08/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TicTacToe.
+FOUNDATION_EXPORT double TicTacToeVersionNumber;
+
+//! Project version string for TicTacToe.
+FOUNDATION_EXPORT const unsigned char TicTacToeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TicTacToe/PublicHeader.h>
+
+
