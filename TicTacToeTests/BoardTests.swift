a102a621cf21e472aaf92e98fda54b18fc1196a6
Peter Combee <petercombee@gmail.com>
2023-08-11 20:21:30 +0200

    Marking spot updates board state

--- TicTacToeTests/BoardTests.swift
+++ TicTacToeTests/BoardTests.swift
@@ -4,12 +4,31 @@
 
 import XCTest
 
+enum Sign {
+    case x
+}
+
 struct Board {
-    let state: [[Bool?]] = [
+    
+    enum Row {
+        case one
+    }
+    
+    enum Col {
+        case two
+    }
+    
+    var state: [[Sign?]] = [
         [.none, .none, .none],
         [.none, .none, .none],
         [.none, .none, .none]
     ]
+    
+    func mark(row: Row, col: Col, withSign sign: Sign) -> Board {
+        var copy = state
+        copy[0][1] = .x
+        return Board(state: copy)
+    }
 }
 
 final class BoardTests: XCTestCase {
@@ -20,5 +39,20 @@ final class BoardTests: XCTestCase {
             [.none, .none, .none],
             [.none, .none, .none]
         ])
+    }
+    
+    func test_markSpotUpdatesState() {
+        
+        let board = Board()
+        
+        let newState = board.mark(row: .one, col: .two, withSign: .x)
+     
+        let expectedState: [[Sign?]] = [
+            [.none, .x, .none],
+            [.none, .none, .none],
+            [.none, .none, .none]
+        ]
+        
+        XCTAssertEqual(newState.state, expectedState)
     }
 }
