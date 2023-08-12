55d55d8ab12bb03a2d6392b06a4422202b78ba03
Peter Combee <petercombee@gmail.com>
2023-08-12 21:23:43 +0200

    `Game.start` starts game by returning first turn for player `.o`

---
+++ TicTacToeTests/GameTests.swift
@@ -0,0 +1,30 @@
+//
+//  Created by Peter Combee on 12/08/2023.
+//
+
+import XCTest
+
+enum Player {
+    case o
+}
+
+struct Turn {
+    let player: Player
+}
+
+struct Game {
+    func start() -> Turn {
+        Turn(player: .o)
+    }
+}
+
+final class GameTests: XCTestCase {
+    
+    func test() {
+        let game = Game()
+        
+        let turn1 = game.start()
+        
+        XCTAssertEqual(turn1.player, .o)
+    }
+}
