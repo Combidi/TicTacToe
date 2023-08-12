53f28a3d8461722c3f2ff42b0fbf1d6c0b23c16c
Peter Combee <petercombee@gmail.com>
2023-08-12 21:31:43 +0200

    `Game.start` notifies handler with empty board

--- TicTacToeTests/GameTests.swift
+++ TicTacToeTests/GameTests.swift
@@ -13,18 +13,38 @@ struct Turn {
 }
 
 struct Game {
+    
+    private let onBoardStateChange: (Board) -> Void
+     
+    init(onBoardStateChange: @escaping (Board) -> Void) {
+        self.onBoardStateChange = onBoardStateChange
+    }
+    
     func start() -> Turn {
-        Turn(player: .o)
+        onBoardStateChange(Board())
+        return Turn(player: .o)
     }
 }
 
 final class GameTests: XCTestCase {
     
     func test_startsGameForPlayerO() {
-        let game = Game()
+        let game = Game(onBoardStateChange: { _ in })
         
         let turn1 = game.start()
         
         XCTAssertEqual(turn1.player, .o)
+    }
+    
+    func test_startGameNotifiesHandlerWithInitialBoardState() {
+        var capturedBoard: Board?
+        let game = Game(onBoardStateChange: { capturedBoard = $0 })
+
+        XCTAssertNil(capturedBoard)
+
+        _ = game.start()
+
+        let emptyBoard = Board()
+        XCTAssertEqual(capturedBoard?.state, emptyBoard.state)
     }
 }
